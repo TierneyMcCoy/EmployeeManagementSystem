20fd10d1dfd7af663d5df3d7903447747179d486
Tierney.mccoy@gmail.com <Tierney.mccoy@gmail.com>
2026-09-21 21:44:46 -0500

    Initial commit

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Tom";
+            employee.LastName = "Welling";
+
+            string FullName = employee.GetFullName();
+
+            return View(employee);
+
+            // return Content(FullName);
+
+        }
+
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+        public int     Age { get; set; }
+
+
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+    }
+}
